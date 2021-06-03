b1e961c25c83ea4fec602c0bb9f41e4c08e10b0e
Ilyin-Ilya <fate98765@gmail.com>
2021-06-03 09:51:27 +0200

    init

--- main.py
+++ main.py
@@ -1,139 +1,134 @@
 import inline as inline
 import numpy as np
+import math
 import pandas as pd
 import scipy
+from scipy import interpolate
 from sklearn import datasets, linear_model
 import scipy.linalg as sla
 import matplotlib.pyplot as plt
 from scipy import interpolate
 import PySimpleGUI as sg
 
-
-def mse(preds, y):
-    """
-    повертає середньоквадратичну помилку між preds та y.
-    """
-    return ((preds - y)**2).mean()
-
-
-def grad_descent(X, y, lr, num_iter=100):
-    global W, b
-    W = np.random.rand(X.shape[1])
-    b = np.array(np.random.rand(1))
-
-    losses = []
-
-    N = X.shape[0]
-    for iter_num in range(num_iter):
-        preds = predict(X)
-        losses.append(mse(preds, y))
-
-        w_grad = np.zeros_like(W)
-        b_grad = 0
-        for sample, prediction, label in zip(X, preds, y):
-            w_grad += 2 * (prediction - label) * sample
-            b_grad += 2 * (prediction - label)
-
-        W -= lr * w_grad
-        b -= lr * b_grad
-    return losses
-
-
-def generate_data(range_, a, b, std, num_points=100):
-    """Генерує дани, що підпорядковуються залежності y = a*x + b + е,
-    де е - нормально розподілене зі стандартним відхиленням std и нульовим середнім."""
-    X_train = np.random.random(num_points) * (range_[1] - range_[0]) + range_[0]
-    y_train = a * X_train + b + np.random.normal(0, std, size=X_train.shape)
-
-    return X_train, y_train
-
-def predict(X):
-    """
-   Передбачує занчення y, користуючись поточні значення W та b
-    """
-    global W, b
-    g = X @ W + b.reshape(-1, 1)
-    f = np.squeeze(g)
-    return np.squeeze(X @ W + b.reshape(-1, 1))
-
+my_font = ("Helvetica", 13)
 "  y'-2xy=0"
 "  y(0) = 1"
 
+def RepresentsInt(s:str):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
-def solve_weights(X, y):
-    """
-    Обчислює значення W,b методом найменших квадратів для X та y.
-    """
-    global W, b
-
-    N = X.shape[0]
-    # додаємо до ознак фіктивний розмір
-    bias = np.ones((N, 1))
-    X_b = np.append(bias, X, axis=1)
-
-    W_full = np.linalg.inv(X_b.T @ X_b) @ X_b.T @ y
-
-    W = W_full[1:]
-    b = np.array([W_full[0]])
-
-def create_matr(N, X, step):
+def create_matr(N, X, step,y_init):
     matr = list(list())
-    for i in range(0,N-1):
-        my_list=list()
-        for j in range(0,N):
-            if j==i:
-                my_list.append(-2*X[j]-1/step)
-                continue
-            if j==i+1:
-                my_list.append(1/step)
+    for i in range(0, N - len(y_init)):
+        my_list = list()
+        j=0
+        while j< N:
+            if j == i:
+                my_list.append((1 / step)**2 + 1)
+
+                my_list.append(-2*(1 / step)**2)
+
+                my_list.append((1 / step)**2)
+                j += 3
                 continue
             my_list.append(0)
+            j+=1
         matr.append(my_list)
-    list2 = list()
-    list2.append(1)
-    for i in range(1,N):
-        list2.append(0)
-    matr.append(list2)
+
+    for i in range(0,len(y_init)):
+        list2 = list()
+        for j in range(0,N):
+            if j==i:
+                list2.append(1)
+                continue
+            list2.append(0)
+        matr.append(list2)
     return matr
 
-def my_grad():
-    x_begin = 0
-    x_end = 1
-    N=100
-    step = (x_end-x_begin)/N
 
-    X=np.linspace(x_begin,x_end,N)
-    y=np.zeros(N)
+def my_grad(x_begin,x_end,y_init, order,first_field, operation, second_field, func):
+    N = 500
+    step = (x_end - x_begin) / N
 
-    matr = np.array(create_matr(N,X,step))
-    right_side = np.zeros((N,1))
-    right_side[N-1][0]=1
+    X = np.linspace(x_begin, x_end, N)
+    y = np.zeros(N)
+
+    matr = np.array(create_matr(N, X, step,y_init))
+    right_side = np.zeros((N, 1))
+    for i in range(0,N-order):
+        if(func=="tgx"):
+            right_side[i]=math.tan(X[i])
+    for i in range(0,len(y_init)):
+         right_side[N - i - 1][0] = y_init[i]
     solution = np.linalg.solve(matr, right_side)
-
-    plt.plot(X, solution,'o')
+    f=np.rot90(solution)
+    f1 = scipy.interpolate.interp1d(X, f, kind='cubic')
+    plt.plot(X, solution, '-')
     plt.show()
 
-    losses = []
+def parseEquation(equation: str):
+    i=0
+    order=-1
+    first_field=list()
+    second_field = [1,0,0]
+    right_side = "0"
+    while(i<len(equation)):
+        while equation[i] != "+" and equation[i] != "-" and equation[i] != "=" :
+            if equation[i] == "y":
+                i+=1
+                order+=1
+                while equation[i] == "\'":
+                    order += 1
+                    i += 1
+                first_field.append(order)
+                order=0
+        operation = equation[i]
+        i+=1
+        while equation[i]!="=":
+            j=i
+            f = equation[5:6]
+            while RepresentsInt(equation[j:(i+1)]):
+                second_field[0]=int(equation[j:(i+1)])
+                i+=1
+            if equation[i]=="x":
+                second_field[1]=1
+                i+=1
+            if equation[i]=="y":
+                second_field[2]=1
+                i+=1
+        i+=1
+        if (equation[i]=="0"):
+            right_side=0
+        if (equation[i]=="t" and equation[i+1]=="g" and equation[i+2]=="x"):
+            right_side="tgx"
+        i+=1
+    return first_field,operation,second_field,right_side
 
-    N = X.shape[0]
-    for iter_num in range(15000):
-        preds = predict(X)
-        losses.append(mse(preds, y))
-
-        w_grad = np.zeros_like(W)
-        b_grad = 0
-        for sample, prediction, label in zip(X, preds, y):
-            w_grad += 2 * (prediction - label) * sample
-            b_grad += 2 * (prediction - label)
-
-        W -= 0.05 * w_grad
-        b -= 0.05 * b_grad
+def getOrderOfTheEquation(equation: str):
+    res = 0
+    i = 0
+    while i < len(equation):
+        current_res = 0
+        if equation[i] == "\'":
+            while (equation[i] == "\'"):
+                current_res += 1
+                i += 1
+            if (current_res > res):
+                res = current_res
+        i += 1
+    return res
 
 
 def gui(name):
-    general_info = [[sg.Text('Differential equation'),sg.InputText(key="-equation-")],
-                    [sg.Button("Solve the equation", key="-solve-")]]
-    # Create the window
+    'y\'\'y-2xy=0'
+    first_field, operation, second_field, right_side = parseEquation('y\'\'y-2xy=0')
+    general_info = [[sg.Text('Differential equation', font=my_font), sg.InputText(key="-equation-", font=my_font)],
+                    [sg.Button("Add initial conditions", key="-add_initial_conditions-", font=my_font)]]
+    #     # Create the window
     window = sg.Window("DiffEquationSolver", general_info, margins=(400, 300))
 
     # Create an event loop
@@ -143,10 +138,59 @@ def gui(name):
         # presses the OK button
         if event == sg.WIN_CLOSED:
             break
+        if event == "-add_initial_conditions-":
+            equation = values['-equation-']
+            if (equation == ""):
+                sg.popup("Please, input the equation first")
+                continue
+            equation_order = getOrderOfTheEquation(equation)
+            window1 = sg.Window("Differential equation", add_initial_conditions(equation_order, equation),
+                                margins=(400, 300))
+            window.Close()
+            window = window1
+            continue
         if event == "-solve-":
-            number_of_params = values['-equation-']
+            try:
+                init_x, init_y = get_initial_conditions(values)
+            except Exception:
+                sg.popup("Please, fill initial conditions properly. The value can not be interpreted as a number")
+                continue
+            x_begin = min(init_x)
+            x_end=(max(init_x)-x_begin+1)*10
+            first_field, operation, second_field, right_side = parseEquation(equation)
+            my_grad(x_begin,x_end,init_y,equation_order)
+            continue
+        if event == "-amend-":
             my_grad()
+            continue
+
     window.close()
+
+
+def add_initial_conditions(num: int, equation: str):
+    general_info = [[sg.Text(equation, font=my_font)],
+                    [sg.Button("Solve the equation", key="-solve-", font=my_font),
+                     sg.Button("Amend equation", key="-amend-", font=my_font)]]
+    order = ""
+    for i in range(num):
+        general_info[1].append([sg.Text("y" + order + "(", font=my_font),
+                                sg.InputText(key="-coef_" + str(i) + "-", size=(5, 5), font=my_font),
+                                sg.Text(") =", font=my_font),
+                                sg.InputText(key="-value_" + str(i) + "-", size=(5, 5), font=my_font)])
+        order += "'"
+    return general_info
+
+
+def get_initial_conditions(values: dict):
+    coefs = []
+    equals = []
+    for i in range(0, int((len(values) + 1) / 2)):
+        coefs.append(float(values["-coef_" + str(i) + "-"]))
+        equals.append(float(values["-value_" + str(i) + "-"]))
+
+    return coefs, equals
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     gui("")
